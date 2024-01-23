e96cb96302083fb6461ac7229811c2cd39770154
unknown <2daw3@HZ114478.H012108.NET>
2024-01-23 14:18:43 +0100

    contenedores distribucion mejorada

--- contenedores/laravel_api-bbdd/app/Models/User.php
+++ contenedores/laravel_api-bbdd/app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -15,35 +15,31 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
-
     protected $fillable = [
         'name',
         'email',
         'password',
-
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<int, string>
      */
-
     protected $hidden = [
         'password',
         'remember_token',
-
     ];
 
     /**
      * The attributes that should be cast.
      *
-     * @var array
+     * @var array<string, string>
      */
-
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 }
--- contenedores/laravel_api-bbdd/config/auth.php
+++ contenedores/laravel_api-bbdd/config/auth.php
@@ -40,13 +40,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'api' => [
-
-            'driver' => 'passport',
-
-            'provider' => 'users',
-
-        ],
     ],
 
     /*
