091ff2daadf8dfa0fe6207451ec570d115e22311
unknown <2daw3@HZ114478.H012108.NET>
2024-01-24 14:21:15 +0100

    login, register, medio implementados

--- contenedores/laravel_api-bbdd/routes/api.php
+++ contenedores/laravel_api-bbdd/routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
 
+  
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+Route::post('logout', [RegisterController::class, 'logout']);
--- contenedores/pagina_web/register.js
+++ contenedores/pagina_web/register.js
@@ -1,6 +1,36 @@
-function logout() {
-    
+let formRegister = document.getElementById("registerform")
+formRegister.addEventListener("submit", event => {
+    event.preventDefault()
+    register()
+})
+
+let formLogin = document.getElementById("loginform")
+formLogin.addEventListener("submit", event => {
+    event.preventDefault()
+    login()
+})
+
+
+
+
+function registrarAbrir() {
+    let register = document.getElementById("registerform");
+    let login = document.getElementById("loginform");
+
+    register.style.display = "block"
+    login.style.display = "none"
+
 }
+
+function abrirFormLogin() {
+    let register = document.getElementById("registerform");
+    let login = document.getElementById("loginform");
+
+    register.style.display = "none"
+    login.style.display = "block"
+
+}
+
 
 function register() {
     let data = {
@@ -17,22 +47,24 @@ function register() {
         },
         body: JSON.stringify(data)
     }
+    console.log(config)
+    console.log(data)
 
-    fetch("http://localhost:8086/api/register", config)
+    fetch("http://localhost:8081/api/register", config)
         .then(response => {
             if (!response.ok) {
                 throw new Error("La solicitud no se pudo completar correctamente.");
             }
             return response.json();
         })
-        .then(data=>{
+        .then(data => {
             console.log(data)
         })
 }
 
 function login() {
     let data = {
-        email: document.getElementById("email").value,
+        email: document.getElementById("correo").value,
         password: document.getElementById("contrasena").value,
     }
     let config = {
@@ -43,14 +75,47 @@ function login() {
         body: JSON.stringify(data)
     }
 
-    fetch("http://localhost:8086/api/login", config)
+    console.log(config)
+    console.log(data)
+    setTimeout(() => {
+        fetch("http://localhost:8081/api/login", config)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("La solicitud no se pudo completar correctamente.");
+                }
+                return response.json();
+            })
+            .then(data => {
+                localStorage.setItem("token",data["data"]["token"])
+                
+            })
+    }, 2000);
+
+
+}
+
+
+function logout() {
+    let data = {
+        name: document.getElementById("usuario").value,
+    }
+
+    let config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    fetch("http://localhost:8081/api/logout", config)
         .then(response => {
             if (!response.ok) {
                 throw new Error("La solicitud no se pudo completar correctamente.");
             }
             return response.json();
         })
-        .then(data=>{
-            console.log(data)
+        .then(data => {
+            localStorage.clear("token")
         })
 }
